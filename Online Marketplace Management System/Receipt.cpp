ecb1841e9deb82bbe02358c26c437a4731c4dfdc
haitham2001 <m.haitham2001@gmail.com>
2021-05-24 15:19:35 +0200

    Added class members, constructors and getters for receipt

--- Online Marketplace Management System/Receipt.cpp
+++ Online Marketplace Management System/Receipt.cpp
@@ -1,2 +1,28 @@
-
 #include "Receipt.h"
+
+Receipt::Receipt(float total_price, float shipment_price)
+{
+	this->total_price = total_price;
+	this->shipment_price = shipment_price;
+}
+
+Receipt::Receipt(float total_price)
+{
+	this->total_price = total_price;
+	shipment_price = 30.0f;
+}
+
+int Receipt::get_id()
+{
+	return id;
+}
+
+float Receipt::get_total_price()
+{
+	return total_price;
+}
+
+float Receipt::get_shipment_price()
+{
+	return shipment_price;
+}
--- Online Marketplace Management System/Receipt.h
+++ Online Marketplace Management System/Receipt.h
@@ -1,5 +1,25 @@
 #pragma once
+#include <iostream>
+#include "Product.h"
+#include "Customer.h"
+
+using namespace std;
+
 class Receipt
 {
+private:
+	int id;
+	float total_price;
+	float shipment_price;
+	Customer cust;
+
+public:
+	Receipt(float total_price, float shipment_price);
+	Receipt(float total_price);
+
+	int get_id();
+	float get_total_price();
+	float get_shipment_price();
+
 };
 
