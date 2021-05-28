86e7abc79a3b56f13bf2bd866890e80d9666195f
HadyAhmed00 <61879050+HadyAhmed00@users.noreply.github.com>
2021-05-28 12:52:11 +0200

    Optimizing the main code a little bet

--- Online Marketplace Management System/main.cpp
+++ Online Marketplace Management System/main.cpp
@@ -8,40 +8,21 @@
 #include<string>
 #include"Validation.h"
 using namespace std;
+//------------------ global vars and vectors ------------------
 
-//password function
-string password() {
-    char pass[50];
-    int num = 0;
-    cout << "Enter the password: \n";
-    while (pass[num - 1] != '\r')// what does \r mean
-    {
-        pass[num] = _getch();
-        if (pass[num - 1] != '\r')
-        {
-            cout << "*";
-        }
-        num++;
-    }
-    pass[num - 1] = '\0';
-    string s = pass;
-    return s;
-}
+vector <Customer> customers; //all customers in system
+vector <Product> products; //all products in system
+vector <Seller> sellers; //all sellers in system
 
-
-
-
-
+//-------------------------function declaration --------------------
+string password();
 void control(Person, vector <Customer>&, vector <Seller>&);
 void Registration(Person, vector <Customer>&, vector <Seller>&);
 void login(Person, vector <Customer>&, vector <Seller>&);
 
-//start main<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 int main()
 {
-    vector <Customer> customers; //all customers in system
-    vector <Product> products; //all products in system
-    vector <Seller> sellers; //all sellers in system
+    
 
     Product p1(1, 5, 15, "phone", "a");
     Product p2(5, 5, 15, "android", "b");
@@ -49,33 +30,57 @@ int main()
     Product p4(5, 5, 15, "lenovo", "c");
 
     Person per;
-
     //customers
     //the information for the first customer
-    Customer c1("Customer1", "Customer1@gmail.com", "Customer1 adress", "phone1", "pass1");
-    //the information for the second customer
-    Customer c2("Customer2", "Customer2@gmail.com", "Customer1 adress", "phone2", "pass2");
+    Customer c1("Customer1",			       //Name
+				"Customer1@gmail.com",		  //Email
+				"Customer1 adress",			 //adress
+				"phone1",				    //phone
+				"pass1");				   //passwod
+
+	//the information for the second customer
+    Customer c2("Customer2",                 //Name
+				"Customer2@gmail.com", 	    //Email
+				"Customer1 adress",		   //adress
+				"phone2", 				  //phone
+				"pass2");				 //passwod
     //save the data
     customers.push_back(c1);
     customers.push_back(c2);
     //sellers
+
     //the information for the first seller
-    Seller s1("Seller1", "pass3", "Seller1@gmail.com", "phone3", "Seller1 adress");
+    Seller s1("Seller1", //Name
+			  "pass3",  //password
+			  "Seller1@gmail.com",  //Email
+			  "phone3",      // NO NEED TO THIS IN SELLER
+			  "Seller1 adress"); //NO NEED TO THIS IN SELLER
+
     //the information for the second seller
-    Seller s2("Seller2", "pass4", "Seller2@gmail.com", "phone4", "Seller2 adress");
+    Seller s2("Seller2",  //Name
+			  "pass4", 	 //password
+			  "Seller2@gmail.com",   //Email
+			  "phone4",			// NO NEED TO THIS IN SELLER
+			  "Seller2 adress");	//NO NEED TO THIS IN SELLER
     //save the data
     sellers.push_back(s1);
     sellers.push_back(s2);
     //to display the data in seller and customer vector
     for (int i = 0; i < sellers.size(); i++)
-        cout << sellers[i].get_name() << "\t\t" << sellers[i].get_password() << "\t\t" << sellers[i].get_id() << "\t\t" << sellers[i].get_email() << "\t\t" << sellers[i].get_address() << "\t\t" << sellers[i].get_phoneNum() << endl;
+        cout << sellers[i].get_name() << "\t\t" <<
+				sellers[i].get_password() << "\t\t" <<
+				sellers[i].get_id() <<"\t\t" << 
+				sellers[i].get_email() << "\t\t" << 
+				sellers[i].get_address() << "\t\t" << 
+				sellers[i].get_phoneNum() << endl;
+
     for (int i = 0; i < customers.size(); i++)
-        cout << customers[i].get_name() << "\t" << customers[i].get_password() << "\t\t" << customers[i].get_id() << "\t\t" << customers[i].get_email() << "\t\t" << customers[i].get_address() << "\t" << customers[i].get_phoneNum() << endl;
-
-
-    //#########################################################################################################################################################
-
-
+        cout << customers[i].get_name() << "\t" << 
+				customers[i].get_password() << "\t\t" << 
+				customers[i].get_id() << "\t\t" << 
+				customers[i].get_email() << "\t\t" <<
+				customers[i].get_address() << "\t" << 
+				customers[i].get_phoneNum() << endl;
 
     cout << "\n\n\n";
     cout << "Online Marketplace Management System\n";
@@ -100,30 +105,39 @@ int main()
             cout << "invalid\n";
         }
     }
-    
-
-   
-    
-    
     system("pause");
     return 0;
 
 }
 
-//end main >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-
+//------------------- function Defenation --------------------
+//password function
+string password() {
+	char pass[50];
+	int num = 0;
+	cout << "Enter the password: \n";
+	while (pass[num - 1] != '\r')// what does \r mean
+	{
+		pass[num] = _getch();
+		if (pass[num - 1] != '\r')
+		{
+			cout << "*";
+		}
+		num++;
+	}
+	pass[num - 1] = '\0';
+	string s = pass;
+	return s;
+}
 
 //for asking the user if he want to login or regist
-
 void control(Person per, vector <Customer>& customers, vector <Seller>& sellers)
 {
     //Person p;
     int choice;   //this variable is entered by the user to choose his navigation 
-
     cout << "please enter number :\n";
     cout << "1-Login\t" << "2-Registration\n";
     cin >> choice;
-
     if (choice == 1)
     {
         login(per, customers, sellers);
@@ -131,10 +145,22 @@ void control(Person per, vector <Customer>& customers, vector <Seller>& sellers)
     else if (choice == 2)
     {
         Registration(per, customers, sellers);
+
         for (int i = 0; i < sellers.size(); i++)
-            cout << sellers[i].get_name() << "\t\t" << sellers[i].get_password() << "\t\t" << sellers[i].get_id() << "\t\t" << sellers[i].get_email() << "\t\t" << sellers[i].get_address() << "\t\t" << sellers[i].get_phoneNum() << endl;
+            cout << sellers[i].get_name() << "\t\t" << 
+					sellers[i].get_password() << "\t\t" <<
+					sellers[i].get_id() << "\t\t" <<
+					sellers[i].get_email() << "\t\t" << 
+					sellers[i].get_address() << "\t\t" << 
+					sellers[i].get_phoneNum() << endl;
+
         for (int i = 0; i < customers.size(); i++)
-            cout << customers[i].get_name() << "\t" << customers[i].get_password() << "\t\t" << customers[i].get_id() << "\t\t" << customers[i].get_email() << "\t\t" << customers[i].get_address() << "\t" << customers[i].get_phoneNum() << endl;
+            cout << customers[i].get_name() << "\t" << 
+					customers[i].get_password() << "\t\t" <<
+					customers[i].get_id() << "\t\t" <<
+					customers[i].get_email() << "\t\t" <<
+					customers[i].get_address() << "\t" << 
+					customers[i].get_phoneNum() << endl;
     }
     else
     {
@@ -195,15 +221,24 @@ void Registration(Person per, vector <Customer>& customers, vector <Seller>& sel
 
     if (number == 2)
     {
-        Customer c(per.get_name(), per.get_email(), per.get_address(), per.get_phoneNum(), per.get_password()); //temparaory save for customer
+        Customer c(per.get_name(),
+				   per.get_email(), 
+				   per.get_address(), 
+				   per.get_phoneNum(), 
+				   per.get_password()); //temparaory save for customer
+
         customers.push_back(c);
     }
     else if (number == 1)
     {
-        Seller s(per.get_name(), per.get_password(), per.get_email(), per.get_phoneNum(), per.get_address());//temparaory save for seller
+        Seller s(per.get_name(),
+				 per.get_password(), 
+				 per.get_email(),
+				 per.get_phoneNum(), 
+				 per.get_address());//temparaory save for seller
+
         sellers.push_back(s);
     }
-
 }
 
 //login functiom
@@ -223,7 +258,8 @@ void login(Person per, vector <Customer>& customers, vector <Seller>& sellers)
     {
         for (int i = 0; i < sellers.size(); i++)
         {
-            if (person_email == sellers[i].getEmail() && person_password == sellers[i].get_password())
+            if (person_email == sellers[i].getEmail() && 
+				person_password == sellers[i].get_password())
             {
                 cout << "\nhello , " << sellers[i].get_name() << endl;
                 sellers[i].seller_menu();
@@ -232,7 +268,6 @@ void login(Person per, vector <Customer>& customers, vector <Seller>& sellers)
                 break;
             }
             check = false; //why put check = false
-
         }
         if (check == false)
         {
@@ -245,7 +280,8 @@ void login(Person per, vector <Customer>& customers, vector <Seller>& sellers)
     {
         for (int i = 0; i < customers.size(); i++)
         {
-            if (person_email == customers[i].get_email() && person_password == customers[i].get_password())
+            if (person_email == customers[i].get_email() && 
+				person_password == customers[i].get_password())
             {
                 cout << "\nhello, " << customers[i].get_name() << endl;
                 check = true;
@@ -253,7 +289,6 @@ void login(Person per, vector <Customer>& customers, vector <Seller>& sellers)
                 break;
             }
             check = false;
-
         }
         if (check == false)
         {
